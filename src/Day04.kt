b0e66f17146ab42ede69d574d78508d46cd3829a
Matthew Lievens <mattlievens@gmail.com>
2024-12-04 00:57:44 -0600

    Day4 solution:
    Part 1: 2336
    Part 2: 1831

---
+++ src/Day04.kt
@@ -0,0 +1,152 @@
+typealias Grid = List<List<Char>>
+
+fun main() {
+    val input = readInput("Day04").createGrid()
+    println("Part 1: ${Day04.Part1.run(input)}")
+    println("Part 2: ${Day04.Part2.run(input)}")
+}
+
+fun List<String>.createGrid(): Grid {
+    val stringList = this
+    return buildList {
+        stringList.forEach { row ->
+            add(row.toCharArray().toList())
+        }
+    }
+}
+
+private object Day04 {
+    object Part1 {
+        fun run(input: Grid): Int {
+            return getXMasCount(input)
+        }
+
+        private fun getXMasCount(grid: Grid): Int {
+            var total = 0
+            grid.forEachIndexed { xIndex, row ->
+                row.forEachIndexed { yIndex, value ->
+                    if (value == 'X') {
+                        total += checkPoint(xIndex, yIndex, grid)
+                    }
+                }
+            }
+            return total
+        }
+
+        private fun checkPoint(xIndex: Int, yIndex: Int, input: Grid): Int {
+            var total = 0
+            for (direction in Direction.entries) {
+                if (direction.isXMas(xIndex, yIndex, input)) {
+                    total++
+                }
+            }
+            return total
+        }
+
+        fun Direction.isXMas(xIndex: Int, yIndex: Int, input: Grid): Boolean {
+            val mPair: Pair<Int, Int>
+            val aPair: Pair<Int, Int>
+            val sPair: Pair<Int, Int>
+            when (this) {
+                Direction.UP -> {
+                    mPair = xIndex to yIndex - 1
+                    aPair = xIndex to yIndex - 2
+                    sPair = xIndex to yIndex - 3
+                }
+                Direction.DOWN -> {
+                    mPair = xIndex to yIndex + 1
+                    aPair = xIndex to yIndex + 2
+                    sPair = xIndex to yIndex + 3
+                }
+                Direction.LEFT -> {
+                    mPair = xIndex - 1 to yIndex
+                    aPair = xIndex - 2 to yIndex
+                    sPair = xIndex - 3 to yIndex
+                }
+                Direction.RIGHT -> {
+                    mPair = xIndex + 1 to yIndex
+                    aPair = xIndex + 2 to yIndex
+                    sPair = xIndex + 3 to yIndex
+                }
+                Direction.UL -> {
+                    mPair = xIndex - 1 to yIndex - 1
+                    aPair = xIndex - 2 to yIndex - 2
+                    sPair = xIndex - 3 to yIndex - 3
+                }
+                Direction.UR -> {
+                    mPair = xIndex + 1 to yIndex - 1
+                    aPair = xIndex + 2 to yIndex - 2
+                    sPair = xIndex + 3 to yIndex - 3
+                }
+                Direction.DL -> {
+                    mPair = xIndex - 1 to yIndex + 1
+                    aPair = xIndex - 2 to yIndex + 2
+                    sPair = xIndex - 3 to yIndex + 3
+                }
+                Direction.DR -> {
+                    mPair = xIndex + 1 to yIndex + 1
+                    aPair = xIndex + 2 to yIndex + 2
+                    sPair = xIndex + 3 to yIndex + 3
+                }
+            }
+            return try {
+                input[mPair.first][mPair.second] == 'M'
+                        && input[aPair.first][aPair.second] == 'A'
+                        && input[sPair.first][sPair.second] == 'S'
+            } catch(e: Exception) {
+                false
+            }
+        }
+    }
+
+    object Part2 {
+        fun run(input: Grid): Int {
+            return getXMasCount(input)
+        }
+
+        private fun getXMasCount(grid: Grid): Int {
+            var total = 0
+            grid.forEachIndexed { xIndex, row ->
+                row.forEachIndexed { yIndex, value ->
+                    try {
+                        if (value == 'A' && checkPoint(xIndex, yIndex, grid)) {
+                            total++
+                        }
+                    } catch (_: Exception) {
+
+                    }
+                }
+            }
+            return total
+        }
+
+        private fun checkPoint(xIndex: Int, yIndex: Int, input: Grid): Boolean {
+            val ul = input[xIndex-1][yIndex-1]
+            val ur = input[xIndex+1][yIndex-1]
+            val dl = input[xIndex-1][yIndex+1]
+            val dr = input[xIndex+1][yIndex+1]
+            val corners = buildList {
+                add(ul)
+                add(ur)
+                add(dl)
+                add(dr)
+            }
+
+            return !(corners.contains('X') || corners.contains('A') || ul == dr || dl == ur)
+        }
+
+    }
+
+    enum class Direction {
+        UP,
+        DOWN,
+        LEFT,
+        RIGHT,
+        UL,
+        UR,
+        DL,
+        DR
+    }
+
+
+}
--- src/Utils.kt
+++ src/Utils.kt
@@ -1,5 +1,3 @@
-import java.math.BigInteger
-import java.security.MessageDigest
 import kotlin.io.path.Path
 import kotlin.io.path.readText
 
@@ -11,9 +9,9 @@ fun readInput(name: String) = Path("src/inputs/$name.txt").readText().trim().lin
 /**
  * Converts string to md5 hash.
  */
-fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
-    .toString(16)
-    .padStart(32, '0')
+//fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
+//    .toString(16)
+//    .padStart(32, '0')
 
 /**
  * The cleaner shorthand for printing output.
