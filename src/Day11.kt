f239434ec0d5707a071fd55a3036d393c5b613d8
Matthew Lievens <mattlievens@gmail.com>
2024-12-11 16:13:51 -0600

    Day 11 - Using a cache
    Part 1: 183435
    Part 2: 218279375708592
    
    Fucking Int overflows man

--- src/Day11.kt
+++ src/Day11.kt
@@ -3,38 +3,38 @@ import kotlinx.coroutines.runBlocking
 
 fun main() = runBlocking {
     val input = readInput("Day11").joinToString(" ").split(" ")
-    val blinks = Day11.Blinks(input)
+    val blinks = Day11.Blinks(input.map { it.toLong() })
     println("Part 1: ${blinks.getCount(25)}")
     println("Part 2: ${blinks.getCount(75)}")
 }
 
 private object Day11 {
 
-    class Blinks(val initialStones: List<String>, ) {
-        var foundValues = mutableMapOf<Pair<String, Int>, Int>()
+    class Blinks(val initialStones: List<Long>, ) {
+        var foundValues = mutableMapOf<Pair<Long, Int>, Long>()
 
-        fun getCount(numberOfBlinks: Int) : Int {
+        fun getCount(numberOfBlinks: Int) : Long {
             foundValues = mutableMapOf()
             return initialStones.sumOf {
                 getStoneCount(numberOfBlinks, it)
             }
         }
 
-        fun getStoneCount(currentDepth: Int, stone: String): Int {
+        fun getStoneCount(currentDepth: Int, stone: Long): Long {
             val value = foundValues.getOrPut(stone to currentDepth) {
                 val nextDepth = currentDepth - 1
-                val length = stone.length
                 if (currentDepth <= 0) {
                     1
-                } else if (stone == "0") {
-                    getStoneCount(nextDepth, "1")
-                } else if (length % 2 == 0) {
-                    val half = length / 2
-                    listOf(stone.substring(0, half), stone.substring(half).toLong().toString()).sumOf {
+                } else if (stone == 0L) {
+                    getStoneCount(nextDepth, 1L)
+                } else if (stone.toString().length % 2 == 0) {
+                    val stoneString = stone.toString()
+                    val half = stoneString.length / 2
+                    listOf(stoneString.substring(0, half).toLong(), stoneString.substring(half).toLong()).sumOf {
                         getStoneCount(nextDepth, it)
                     }
                 } else {
-                    getStoneCount(nextDepth, (stone.toLong() * 2024).toString())
+                    getStoneCount(nextDepth, (stone* 2024))
                 }
             }
 
