555bfeeb7b36d43b42619f6f02b3677bbbdcd129
Matthew Lievens <mattlievens@gmail.com>
2024-12-11 16:05:06 -0600

    Day 11 - Using a cache
    Part 1: 183435
    Part 2: 547791280  To low

--- src/Day11.kt
+++ src/Day11.kt
@@ -1,60 +1,43 @@
-import Day11.Part1
-import Day11.Part2
-import Day11.Part2.getStonesAfterBlink
-import kotlinx.coroutines.Deferred
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
-import kotlinx.coroutines.awaitAll
+
 import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.withContext
 
 fun main() = runBlocking {
     val input = readInput("Day11").joinToString(" ").split(" ")
-    println("Part 1: ${Part1.run(input)}")
-    println("Part 2: ${Part2.run(input)}")
+    val blinks = Day11.Blinks(input)
+    println("Part 1: ${blinks.getCount(25)}")
+    println("Part 2: ${blinks.getCount(75)}")
 }
 
 private object Day11 {
-    object Part1 {
-        suspend fun run(input: List<String>): Int {
-            return getStonesAfterBlink(input, 25)
-        }
-    }
 
-    object Part2 {
-        suspend fun run(input: List<String>): Int {
-             return getStonesAfterBlink(input, 75)
+    class Blinks(val initialStones: List<String>, ) {
+        var foundValues = mutableMapOf<Pair<String, Int>, Int>()
+
+        fun getCount(numberOfBlinks: Int) : Int {
+            foundValues = mutableMapOf()
+            return initialStones.sumOf {
+                getStoneCount(numberOfBlinks, it)
+            }
         }
 
-        suspend fun getStonesAfterBlink(stones: List<String>, numberOfBlinks: Int) : Int = withContext(Dispatchers.IO) {
-            val deferred: MutableList<Deferred<Int>> = mutableListOf()
-            stones.forEach {
-                deferred.add(
-                    async {
-                        getStoneCount(numberOfBlinks, 0, it)
+        fun getStoneCount(currentDepth: Int, stone: String): Int {
+            val value = foundValues.getOrPut(stone to currentDepth) {
+                val nextDepth = currentDepth - 1
+                val length = stone.length
+                if (currentDepth <= 0) {
+                    1
+                } else if (stone == "0") {
+                    getStoneCount(nextDepth, "1")
+                } else if (length % 2 == 0) {
+                    val half = length / 2
+                    listOf(stone.substring(0, half), stone.substring(half).toLong().toString()).sumOf {
+                        getStoneCount(nextDepth, it)
                     }
-                )
+                } else {
+                    getStoneCount(nextDepth, (stone.toLong() * 2024).toString())
+                }
             }
-            deferred.awaitAll().sum()
-        }
 
-        fun getStoneCount(maxDepth: Int, currentDepth: Int, stone: String): Int {
-            if (currentDepth >= maxDepth) {
-                return 1
-            }
-            val nextDepth = currentDepth + 1
-            val length = stone.length
-            val value = if (stone == "0") {
-                getStoneCount(maxDepth, nextDepth, "1")
-            } else if (length % 2 == 0) {
-                val half = length / 2
-                val leftStone = stone.substring(0, half)
-                val rightStone = stone.substring(half, stone.length).toLong().toString()
-                getStoneCount(maxDepth, nextDepth, leftStone) +
-                        getStoneCount(maxDepth, nextDepth, rightStone)
-            } else {
-                getStoneCount(maxDepth, nextDepth, (stone.toLong() * 2024).toString())
-            }
             return value
         }
     }
